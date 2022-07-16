666e579870230c3eba934735933f61dfaaf35b35
marinaceccon1 <107349467+marinaceccon1@users.noreply.github.com>
2022-07-16 13:59:26 +0200

    Update and rename generate_test2.py to generate_test3.py

--- generate_test3.py
+++ generate_test3.py
@@ -5,7 +5,7 @@ os.chdir("/content/data/test/")
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
         image_files.append("/content/data/test/" + filename)
-os.chdir("..")
+os.chdir("/content/data/")
 with open("test.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
