cb02058681049249898e00a0d096aa6e415f5325
marinaceccon1 <107349467+marinaceccon1@users.noreply.github.com>
2022-07-16 14:07:54 +0200

    Update and rename generate_train4.py to generate_train5.py

--- generate_train5.py
+++ generate_train5.py
@@ -6,7 +6,7 @@ for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
         image_files.append("/content/data/obj/" + filename)
 os.chdir("/content/data/")
-with open("train.txt", "w") as outfile:
+with open("/content/data/train.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
