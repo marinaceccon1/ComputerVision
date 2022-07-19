10e2ab5281fd5e4a4bc01b2e0effae6c87d7a1e6
marinaceccon1 <107349467+marinaceccon1@users.noreply.github.com>
2022-07-19 20:54:33 +0200

    Update generate_test.py

--- generate_test.py
+++ generate_test.py
@@ -1,10 +1,10 @@
 import os
 
 image_files = []
-os.chdir("/content/data/test")
+os.chdir("/content/drive/MyDrive/Datasets/data/test")
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("/content/data/test/" + filename)
+        image_files.append("/content/drive/MyDrive/Datasets/data/test" + filename)
 os.chdir("..")
 with open("test.txt", "w") as outfile:
     for image in image_files:
